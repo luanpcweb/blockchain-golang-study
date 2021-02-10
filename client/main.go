75cd8a9ad9606d97f4e54b7f35824175d8214c2f
luan.coelho <luan.coelho@webjump.com.br>
2021-02-10 17:19:11 -0300

    Fisrt commit

---
+++ client/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"github.com/plutov/packagemain/00-grpc/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var client proto.BlockchainClient
+
+func main() {
+	addFlag := flag.Bool("add", false, "add new block")
+	listFlag := flag.Bool("list", false, "get the blockchain")
+	flag.Parse()
+
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("cannot dial server: %v", err)
+	}
+
+	client = proto.NewBlockchainClient(conn)
+
+	if *addFlag {
+		addBlock()
+	}
+
+	if *listFlag {
+		getBlockchain()
+	}
+
+}
+
+func addBlock() {
+	block, err := client.AddBlock(context.Background(), &proto.AddBlockRequest{
+		Data: time.Now().String(),
+	})
+
+	if err != nil {
+		log.Fatalf("unable to add block: %v", err)
+	}
+
+	log.Printf("New block hash %s \n", block.Hash)
+
+}
+
+func getBlockchain() {
+	bc, err := client.GetBlockchain(context.Background(), &proto.GetBlockchainRequest{})
+
+	if err != nil {
+		log.Fatalf("unable to get block: %v", err)
+	}
+
+	log.Println("Blocks:")
+	for _, b := range bc.Blocks {
+		log.Printf("hash: %s, prev block hash: %s, data: %s", b.Hash, b.PrevBlockHash, b.Data)
+	}
+}
